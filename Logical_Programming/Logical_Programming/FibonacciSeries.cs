9a034d4b3a38a8906505dd176382a5c13ba6ad19
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-20 23:58:22 +0530

    [Sandeep] Add: Fibonacci series

---
+++ Logical_Programming/Logical_Programming/FibonacciSeries.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    public class FibonacciSeries
+    {
+        public static void FindFibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a + " ");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
---
+++ Logical_Programming/Logical_Programming/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to logical programming problem");
+            Console.WriteLine("Please choose 1program from below option");
+            Console.WriteLine("1.Fibonacci series");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter Value");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries.FindFibonacci(n);
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
